3007573705c17513cfbdb36273088c8a0c92152d
leadersgroup <ericatl828@gmail.com>
2025-09-20 11:19:27 -0400

    rw3

--- api/test.js
+++ api/test.js
@@ -1,4 +1,4 @@
-// api/test.js - Simple test endpoint
+// api/test.js - Simple test endpoint lee shen
 module.exports = async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Content-Type', 'application/json');
